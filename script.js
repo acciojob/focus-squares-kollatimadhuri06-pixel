77be75c2632130494156568d7ed0ce29a87d424d
kollatimadhuri06-pixel <kollatimadhuri06@gmail.com>
2026-05-14 11:24:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 
 
+let boxes=document.getElementsByClassName("square")
+for(let box of boxes){
+	box.addEventListener("mouseenter",()=>{
+		for(item of boxes){
+			if(box!==item)
+			item.classList.add("active")
+		}
+	
+	})
+
+	box.addEventListener("mouseout",()=>{
+		for(item of boxes){
+			if(box!==item)
+			item.classList.remove("active")
+		}
+		})
+}
