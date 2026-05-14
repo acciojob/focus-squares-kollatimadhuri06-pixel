d9d8850615cb1eae2d020e70d4c6c007c318d9d2
kollatimadhuri06-pixel <kollatimadhuri06@gmail.com>
2026-05-14 11:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
-//your JS code here. If required.
- let boxes=document.getElementsByClass("square")
-for(let box of boxes){
-	box.addEventListener("mouseenter",()=>{
-		for(item of boxes){
-			if(box!==item)
-			item.ClassList.add("active")
-		}
-	}
-	})
- let boxes=document.getElementsByClass("square")
-for(let box of boxes){
-	box.addEventListener("mousein",()=>{
-		for(item of boxes){
-			if(box!==item)
-			item.ClassList.remove("active")
-	
+
 
